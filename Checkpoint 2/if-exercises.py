bed677257d0d97b17f8d1eca715952d5792d8a08
Shensi Chinolla <chinollashensi@gmail.com>
2025-08-21 20:20:34 +0000

    if-exercises.py

---
+++ Checkpoint 2/if-exercises.py
@@ -0,0 +1,15 @@
+def main():
+    number = int(input("Please enter a number "))
+    if number < 0:
+        print(number * -1)
+    if number > 0:
+        print(number)
+    
+main()
+
+#Excersice 2
+def operation():
+    number1 = int(input("Type a number "))
+    number2 = int(input("Type another number"))
+
+main()
