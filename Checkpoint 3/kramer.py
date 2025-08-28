54799012872f72a52c37c9b0533199e9fd96366a
Shensi Chinolla <chinollashensi@gmail.com>
2025-08-28 16:20:30 +0000

    kramer.py

--- Checkpoint 3/kramer.py
+++ Checkpoint 3/kramer.py
@@ -1,12 +1,11 @@
-done = False
-while not done:
+while True:
     greeting = input("Type a greeting: ")
 
-if greeting.startswith("hello"):
-    print("You won $0.00 dollars")
-elif greeting.startswith("h"):
-    print("You won $20.00 dollars")
+    if greeting.startswith("hello"):
+        print("You won $0.00 dollars")
+    elif greeting.startswith("h"):
+        print("You won $20.00 dollars")
+    else:
+        print("You won $100.00 dollars")
+        break
 
-else:
-    print("You won $100.00 dollars")
-    done = True 
