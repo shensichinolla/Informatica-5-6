55a2bcd2e1749540ff46c33369986d00ba560bfc
Shensi Chinolla <chinollashensi@gmail.com>
2025-08-29 17:30:06 +0000

    life-loop.py

--- Checkpoint 3/life-loop.py
+++ Checkpoint 3/life-loop.py
@@ -1,1 +1,10 @@
-import time
+import time 
+excersice = input("Did you excersice? ")
+
+while True: 
+    if excersice == ("yes")(1):
+        print(F"Good job, remember to drink water!")
+        break
+    elif excersice== ("no")(0.5):
+        print(F"You will have to catch up tomorrow!")
+        break
