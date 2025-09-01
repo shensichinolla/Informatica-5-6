10b5dd05d0c4ab1556d9a1dabf12c4649664dba1
Shensi Chinolla <chinollashensi@gmail.com>
2025-09-01 19:37:40 +0000

    adv-strings.py

---
+++ Checkpoint 4/adv-strings.py
@@ -0,0 +1,26 @@
+#String length
+input_string = input("Please type a message: ")
+# print(input_string)
+
+# print("-" * len(input_string))
+
+# #String Index
+# print(input_string[0])
+# print(input_string[1])
+# print(input_string[-1])
+
+#Example
+# i = 0
+# while i < len(input_string):
+#     print(input_string[i])
+#     i += 1
+
+#Example 2
+# i = -1
+# while i < len(input_string):
+#     print(input_string[i])
+#     i += -1
+
+print("h" in input_string)
+print("j" in input_string)
+print("ello" in input_string)
