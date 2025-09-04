aac4b36355e12ef8c312de831dd78941aa38a2bb
Shensi Chinolla <chinollashensi@gmail.com>
2025-09-04 17:30:05 +0000

    multiplication.py

---
+++ Checkpoint 4/multiplication.py
@@ -0,0 +1,16 @@
+def main():
+    integer = int(input("Enter a positive integer:"))
+    multiplication_table(integer)
+    
+def multiplication_table(integer):
+    i = 1
+    while i <= 10:
+        print(integer *  [i])
+        
+
+main()      
+        
+    
+    
+    
+    
