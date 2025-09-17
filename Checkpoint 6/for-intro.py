8d3e9259e43908b86ed53ea7932a2a045819f19a
Shensi Chinolla <chinollashensi@gmail.com>
2025-09-17 20:20:56 +0000

    for-intro.py

---
+++ Checkpoint 6/for-intro.py
@@ -0,0 +1,17 @@
+doctrine = ["Faith", "Repentance","Baptism", "Holy Ghost", "Endure to the end"]
+
+
+# i = 0
+# while i < len(doctrine):
+#     print(doctrine[i])
+#     i += 1
+    
+for principle in doctrine: 
+    print(principle)
+
+
+school_objective = ["Deepen conversion to Jesus Christ and his restored Gospel", "Qualify for the blessings of the Temple", "Prepare for missionary and church service", "Build the kingdom of God" ]    
+
+for objective in school_objective:
+    print(objective)
+    print(...)
