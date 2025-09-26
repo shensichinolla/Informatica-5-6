64e1c5e9e1bb610ec60b0b11d1b4c6071f25addc
Shensi Chinolla <chinollashensi@gmail.com>
2025-09-26 17:25:15 +0000

    birthdays.py

--- Checkpoint 7/birthdays.py
+++ Checkpoint 7/birthdays.py
@@ -3,6 +3,13 @@ birthdays = {
     "Mellissa" : "March 15th",
     "Renata" : "March 29th"
 }
+
 name = input("Whos birthday do you want to know?: ").capitalize()
 if name in birthdays:
-    print(f"{name}´s birthday is {birthdays[name]}")
+    print(f"{name}´s birthday is {birthdays[name]}") 
+else:
+    print(f"I don´t have that birthday information")
+    new_birthday = input(f"When is  {name}´s bithday?: ").capitalize()
+    birthdays[name] = new_birthday
+    print("Birthday database updated.")
+print(birthdays)
