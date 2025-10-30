696b8dc9e0d4077b9332cb03b578daf9fedf4e35
Shensi Chinolla <chinollashensi@gmail.com>
2025-10-30 20:20:12 +0000

    calories.py

---
+++ Checkpoint 10/calories.py
@@ -0,0 +1,27 @@
+def main():
+    food = {
+        "Whole Milk" : 73,
+        "Almond Milk" : 30,
+        "Plain Yogurt" : 77,
+        "Sour Cream" : 26,
+        "Egg" : 75,
+        "Egg White" : 16,
+        "Blue Cheese" : 119,
+        "Cream Cheese" :51,
+        "Dry almonds" : 170,
+        "Dry Pumpkin seeds" : 126,
+        "Black beans" : 113,
+        "Lentils" : 115,
+        "Salmon" : 175,
+        "Chicken" : 20,
+        "Broccoli" : 7,
+        "Corn" : 59,
+        "Watermelon" : 11,
+        "Banana" : 33,
+        "Ranch" : 73,
+        "Cooked quinoa" : 56
+    }
+print("Welcome to food combination!!!")
+first_food = input("Enter your first food choice: ")
+second_food = input("Enter your second food choice: ")
+print("Your choices are", first_food, "and", second_food)
