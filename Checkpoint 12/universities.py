0fb7dc7858f9bc10a1c0a2ebd563b2e023a62749
Shensi Chinolla <chinollashensi@gmail.com>
2025-11-10 19:38:52 +0000

    universities.py

---
+++ Checkpoint 12/universities.py
@@ -0,0 +1,7 @@
+print("Welcome to University Fair!!")
+universities = {
+    "Eastern Arizona Collage":{"careers": 90,
+        "Cost":  4776,
+        "Closes campus distance": 404.4 km}
+    "UACJ": {}                           
+}
