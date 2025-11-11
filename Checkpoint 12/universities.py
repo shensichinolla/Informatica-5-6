a9dc1183706246c9ab0f4ec0448ded484881b414
Shensi Chinolla <chinollashensi@gmail.com>
2025-11-11 16:39:56 +0000

    universities.py

--- Checkpoint 12/universities.py
+++ Checkpoint 12/universities.py
@@ -1,7 +1,27 @@
 print("Welcome to University Fair!!")
+print("Universities available: EAC, UACJ, Tecnologico de Monterrey, La Salle, UTP")
+user = input("Which university do you want to know about?")
+
 universities = {
-    "Eastern Arizona Collage":{"careers": 90,
-        "Cost":  4776,
-        "Closes campus distance": 404.4 km}
-    "UACJ": {}                           
+    "EAC": {"distance": 404.4,"num_majors": 90,"price": 8370},
+    "UACJ": {"distance": 11.3, "num_majors": 57, "price": 3800},
+    "Tecnologico de Monterrey": {"distance": 274.7,"num_majors":45,"price": 135000},
+    "La Salle": {"distance": 299.7, "num_majors": 35,"price":60000},
+    "UTP": {"distance":5, "num_majors": 4, "price": 2505}
 }
+
+def main():
+    if user == "EAC":
+        print(universities["EAC"]["distance"],["num_majors"],["price"])
+    elif user == "UACJ":
+        print(universities["EAC"]["distance"],["num_majors"],["price"])
+    elif user == "Tecnologico de Monterrey":
+        print(universities["EAC"]["distance"],["num_majors"],["price"])
+    elif user == "La Salle":
+        print(universities["EAC"]["distance"],["num_majors"],["price"])
+    elif user == "UTP":
+        print(universities["EAC"]["distance"],["num_majors"],["price"])
+    else:
+        print("That´s not a valid university, sorry.")
+
+main()
