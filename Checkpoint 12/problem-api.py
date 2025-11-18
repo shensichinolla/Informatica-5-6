9c77f4efefc30e77ae4cd6ed57e86eabf2a31a03
Shensi Chinolla <chinollashensi@gmail.com>
2025-11-18 16:40:34 +0000

    problem-api.py

--- Checkpoint 12/problem-api.py
+++ Checkpoint 12/problem-api.py
@@ -1,5 +1,14 @@
-import requests 
+import requests
+import random 
 
-def recommend_movie():
-    api = "https://api.themoviedb.org/3/discover/movie"
+def get_movies():
+    url = "https://api.watchmode.com/v1/"
+    api_key = "qzZIdqeeAkLkHgoxh3r1sFLPv2RYtn3DiOZk4BTa"
+    response = requests.get(url)
+    return response.json()
+def pick_random_movie(movies):
+    return random.choice(movies)
+
+def move_info(movie):
+    print(f"Title: ",{movie["title"]})
     
